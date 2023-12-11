1920d3ba2d8b0e8ef9ea9d6eafd1cac408b0ddd7
Abdulbasit <habdulbasit099@gmail.com>
2023-12-11 11:17:02 +0200

    fixed

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -2,6 +2,8 @@ import Cart from "@/components/Cart";
 import Container from "@/components/ui/container";
 import React from "react";
 
+export const revalidate = 0;
+
 const CartPage = () => {
   return (
     <div className="bg-white">
